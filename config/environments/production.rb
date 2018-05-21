102149ba09d1fe26c977dae101f40406c0c97f4c
padovanl <padovan.93@gmail.com>
2018-05-21 17:58:33 +0200

    env change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,5 +93,9 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'obscure-temple-51390.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+
 end
 
