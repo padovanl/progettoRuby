6373461ab6b8b75b4478ad9e1be380e380c759fa
padovanl <padovan.93@gmail.com>
2018-05-23 01:00:32 +0200

    Prova2

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
-        user.skip_confirmation!
       end
     end
   end
@@ -20,6 +19,7 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
       user.image = auth.info.image # assuming the user model has an image
+      user.skip_confirmation!
     end
   end
 end
