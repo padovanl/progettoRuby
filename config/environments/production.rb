7b51d6e03077ddae6cf9bbe263e188439ea29f02
padovanl <padovan.93@gmail.com>
2018-06-19 15:12:43 +0200

    Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.default_url_options = { host: 'https://obscure-temple-51390.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://concorrente2018.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
