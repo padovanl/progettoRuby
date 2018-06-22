a43f5f1d8ad625a50d2febbd509a0287b1ccffe5
borea <a.jizdann@gmail.com>
2018-06-22 09:12:34 +0200

    Aggiunto active storage, con una nuova migration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -62,7 +62,6 @@ Rails.application.configure do
 
 
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'heroku.com' }
   #config.action_mailer.default_url_options = { host: 'localhost' }
   config.action_mailer.delivery_method = :smtp
