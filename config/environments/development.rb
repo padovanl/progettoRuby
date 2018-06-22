035f5a6d31202478dacb33436754d955690e2bf0
borea <a.jizdann@gmail.com>
2018-06-22 15:37:19 +0200

    Cose

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,10 +62,18 @@ Rails.application.configure do
 
 
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'heroku.com' }
-  #config.action_mailer.default_url_options = { host: 'localhost' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
 
+  ActionMailer::Base.smtp_settings = {
+    :user_name => Rails.application.credentials.dig(:sendgrid2, :user_name),
+    :password => Rails.application.credentials.dig(:sendgrid2, :password),
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
 
 end
