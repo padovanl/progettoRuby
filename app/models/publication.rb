fab66c5f99e0180761fbf48d95022a2b0f3de1ee
borea <a.jizdann@gmail.com>
2018-08-01 18:52:20 +0200

    Aggiunto il tmplate a tutte le pagine

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,28 +1,30 @@
 class Publication
-    # model custom per creare form e salvare più model nella stessa chiamata
+  # model custom per creare form e salvare più model nella stessa chiamata
 
-    include ActiveModel::Model
-  
-    attr_accessor :message, :attachments, :user
-  
-    def save
-        return false if invalid?
-    
-        ActiveRecord::Base.transaction do
+  include ActiveModel::Model
 
-            @post = Post.create!(message: message, user: user, course: Course.first())
+  attr_accessor :message, :attachments, :user
 
-            attachments.each do |file|
-                @post.documents.create!(file: file)
-            end
+  def save
+    return false if invalid?
+
+    ActiveRecord::Base.transaction do
+
+      @post = Post.create!(message: message, user: user, course: Course.first())
+
+      unless attachments.nil?
+        attachments.each do |file|
+          @post.documents.create!(file: file)
         end
-    
-        true
-      rescue ActiveRecord::StatementInvalid => e
-        # Handle exception that caused the transaction to fail
-        # e.message and e.cause.message can be helpful
-        errors.add(:base, e.message)
-    
-        false
       end
+    end
+
+    true
+  rescue ActiveRecord::StatementInvalid => e
+    # Handle exception that caused the transaction to fail
+    # e.message and e.cause.message can be helpful
+    errors.add(:base, e.message)
+
+    false
   end
+end
