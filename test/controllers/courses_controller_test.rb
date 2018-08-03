ee703cbc8f4c35a3216c440e06468e5951bd85de
Eugvr <eugenio.balistri@student.unife.it>
2018-08-03 13:13:23 +0200

    aggiunto courses

---
+++ test/controllers/courses_controller_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class CoursesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get courses_url
+    assert true
+  end
+  # test "the truth" do
+  #   assert true
+  # end
+end
