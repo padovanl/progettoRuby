c259bd81c82e6b0f249b80fb4f4afc74e295136f
padovanl <padovan.93@gmail.com>
2018-08-06 01:00:20 +0200

    Aggiunto il nome alla registrazione normale (era obbligatorio), adesso FORSE funziona tutto

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,15 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
   resources :publications, only: [:index]
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
+
 
   resources :documents
   resources :posts do
     resources :comments
   end
+
+  #admin
+  get '/dashboard', to: 'admin#dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
