6a25dc7dbb5ba1e118c382decb060839de0f8d64
padovanl <padovan.93@gmail.com>
2018-08-11 01:58:36 +0200

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,7 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
       user.image = auth.info.image # assuming the user model has an image
+      user.admin = false
       user.skip_confirmation!
     end
   end
