f7ce45dc19f865ccb91586da739f070e5a26e6fa
padovanl <padovan.93@gmail.com>
2018-08-16 14:55:23 +0200

    fix grafico modifica tesi dashboard

--- app/assets/javascripts/components/adminAPI/theses/thesis.jsx
+++ app/assets/javascripts/components/adminAPI/theses/thesis.jsx
@@ -28,7 +28,7 @@ class Thesis extends React.Component{
     render(){
 
         let title = this.state.editable ? <input required={true} type='text'className="input is-medium"  ref={input => this.title = input} defaultValue={this.props.thesis.title}/>:<i>{this.props.thesis.title}</i>;
-        let content = this.state.editable ? <textarea required={true} type='text'className="input is-medium"  ref={input => this.content = input} defaultValue={this.props.thesis.content}/>:<i>{this.props.thesis.content.substring(0,100).concat("...")}</i>;
+        let content = this.state.editable ? <textarea style={{height:300}} required={true} type='text'className="input is-medium"  ref={input => this.content = input} defaultValue={this.props.thesis.content}/>:<i>{this.props.thesis.content.substring(0,100).concat("...")}</i>;
         //let teacher_id = this.state.editable ? <input readOnly="true" required={true} type='text'className="input is-medium"  ref={input => this.teacher_id = input} defaultValue={this.props.thesis.teacher_id}/>:<i>{this.props.thesis.teacher_id}</i>;
         let teacher_id = <i>{this.props.thesis.teacher.name}&nbsp;{this.props.thesis.teacher.surname}</i>;
 
