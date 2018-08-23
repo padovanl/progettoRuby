35f23ad90e315d9be823482448fe900b4b95e123
borea <a.jizdann@gmail.com>
2018-08-23 12:18:18 +0200

    Aggiunto visualizzazione dell'immagine profilo da FB

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,5 +1,18 @@
 class PublicationsController < ApplicationController
   before_action :authenticate_user!
   def index
+    @current_user_avatar = get_avatar_image
+  end
+
+  private
+
+  def get_avatar_image
+    if current_user.avatar.attached?
+      return rails_representation_url(current_user.avatar.variant(resize: "100x100"), only_path: true)
+    elsif current_user.image.nil?
+      return current_user.image
+    else
+      return image_url("dragon.png")
+    end
   end
 end
