127480e574498e8e2176fb116cce4c5f02346305
padovanl <padovan.93@gmail.com>
2018-09-10 03:31:06 +0200

    Sistemato grafica dashboard

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,17 +6,20 @@ class AdminController < ApplicationController
   end
 
   def thesis_tags
-    id_tesi = params[:thesis_]
+    id_tesi = params[:thesis_id]
+    @thesis = Thesis.find(id_tesi)
     render 'thesis_tag'
   end
 
   def courses
     id_d_course = params[:degree_course_id]
+    @degree = DegreeCourse.find(id_d_course)
     render 'courseDashboard'
   end
 
   def teacher_courses
     id_course = params[:course_id]
+    @course = Course.find(id_course)
     render 'admin/teacher_course'
   end
 end
