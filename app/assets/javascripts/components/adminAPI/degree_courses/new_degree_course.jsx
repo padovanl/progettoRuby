9b060e06124cecdaa67ebf01c2d33f92d82e112f
padovanl <padovan.93@gmail.com>
2018-09-15 02:27:04 +0200

    riorganizzato inserimento cdl sulla dashboard

--- app/assets/javascripts/components/adminAPI/degree_courses/new_degree_course.jsx
+++ app/assets/javascripts/components/adminAPI/degree_courses/new_degree_course.jsx
@@ -1,24 +1,38 @@
 const NewDegreeCourse = (props) => {
 
-    let formFields = {}
+    let formFields = {};
+    
+    let style = {
+        marginTop: "1%",
+        marginBottom: "1%",
+        marginRight: "1%",
+        marginLeft: "1%",
+    };
 
+    let hideStyle = {
+        display: "none",
+    };
     return(
-        <tr>
-            <td>
+
+        <div style={hideStyle} id="insertCdlDiv">
+        <fieldset>
+        <legend>Inserisci nuovo CdL</legend>
+            <div style={style}>
                 <input ref={input => formFields.name = input} placeholder='Inserisci il nome del corso di laurea' className="input is-medium" required id="insertNewDegree"/>
-            </td>
-            <td>
+                <br/>
+                <br/>
                 <div className="select">
                     <select ref={input => formFields.tipo = input} className="input is-medium" >
                         <option value="Triennale">Laurea Triennale</option>
                         <option value="Magistrale">Laurea Magistrale</option>
                     </select>
                 </div>
-            </td>
-            <td>
+                <br/>
+                <br/>
                 <a className="button is-rounded is-link is-fullwidth" onClick={ () => props.handleFormSubmit(formFields.name.value, formFields.tipo.value)}>Inserisci</a>
-            </td>
-        </tr>
+            </div>
+            </fieldset>
+        </div>
     )
 
 }
