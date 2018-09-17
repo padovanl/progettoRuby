769dd41b0984f2069fff6142fba5b9edd77adea5
Salvatore Collura <salvatore.collura@student.unife.it>
2018-09-17 11:52:06 +0200

    modifiche varie

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -14,7 +14,7 @@ class NotificationsController < ApplicationController
     #@notifications = Notification.where(recipient: current_user).unread
     respond_to do |format|
       format.html
-      format.json {render json: @notifications, :include => {:recipient => {:only => [:id, :email, :avatar_url_small, :name, :admin]}, :notifiable =>{} } }
+      format.json {render json: @notifications, :include => {:recipient => {:only => [:id, :email, :avatar_url_small, :name, :admin]}, :notifiable =>{:include => {:course=>{}}}}}
     end
   end
 
