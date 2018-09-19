c15a8e3bbb9f9a8a153b767e2f903a2c96914d75
padovanl <padovan.93@gmail.com>
2018-09-19 03:16:55 +0200

    altri test

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class TagTest < ActiveSupport::TestCase
   test "should have a name" do
     tag = Tag.new
+    tag.user = User.first
+    tag.name = ""
     assert_not tag.save
   end
 end
