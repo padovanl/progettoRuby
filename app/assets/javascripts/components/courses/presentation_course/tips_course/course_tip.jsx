282d2fe3839497eb8f85272871590c1d108e0626
Salvatore Collura <salvatore.collura@student.unife.it>
2018-09-27 12:08:25 +0200

    aggiunto elimina suggerimenti per admin

--- app/assets/javascripts/components/courses/presentation_course/tips_course/course_tip.jsx
+++ app/assets/javascripts/components/courses/presentation_course/tips_course/course_tip.jsx
@@ -49,7 +49,7 @@ class CourseTip extends React.Component{
             <tr key={this.props.courseTip.id}>
                 <td className="question_text_column_desktop">{tip_text}</td>
                 {this.props.show_details  ? <td>
-                    { this.props.courseTip.user_id == this.props.user_id && this.props.show_details ? <div> {update_button_item} <span> </span>{delete_button_item} </div> : null}
+                    { (this.props.courseTip.user_id == this.props.current_user.id && this.props.show_details) || this.props.current_user.admin ? <div> {update_button_item} <span> </span>{delete_button_item} </div> : null}
                 </td> : null}
                 <td className="has-text-right"><a title="Reporting" onClick={() => this.props.activeModal(linkReport)}><i className="fas fa-bug"/></a></td>
             </tr>
--- app/assets/javascripts/components/courses/presentation_course/tips_course/main_tip.jsx
+++ app/assets/javascripts/components/courses/presentation_course/tips_course/main_tip.jsx
@@ -7,7 +7,7 @@ class MainTip extends React.Component {
         return(
             <div>
                 <BodyTip course_id={this.props.course_id}
-                         user_id={this.props.user_id}
+                         current_user={this.props.current_user}
                          details_follow_course={this.props.details_follow_course}/>
             </div>
         )
