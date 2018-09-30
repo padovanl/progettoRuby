08d400d2ea61c12e20ef5206fb6d2087da77a77e
Salvatore Collura <salvatore.collura@student.unife.it>
2018-09-30 20:00:02 +0200

    sistemato problema wrap notifiche

--- app/assets/javascripts/components/notifications/navBarNotifications/notification_navbar.jsx
+++ app/assets/javascripts/components/notifications/navBarNotifications/notification_navbar.jsx
@@ -22,6 +22,10 @@ class NotificationNavbar extends React.Component {
             color: '#9e9e9e'
         }
 
+        let a_capo ={
+            whiteSpace: 'normal'
+        }
+
 
         let links = {}
         const actor = this.props.notification.actor.name;
@@ -36,46 +40,46 @@ class NotificationNavbar extends React.Component {
             case 'CourseQuestion':
                 type = 'domanda'
                 link = '/courses/' + id_corso
-                icon = <span className="icon has-text-link" style={style_body_notification}><i className="fas fa-question-circle"></i></span>;
-                scritta_annuncio = <div>{action}<span className="has-text-weight-bold"> {type}</span><div> nel corso di {nome_corso}</div><div className="level"><span className="level-left">{icon}</span><span className="level-right" style={style_color_time}>{notification_time}</span></div></div>
+                icon = <span className="icon has-text-link " style={style_body_notification}><i className="fas fa-question-circle"></i></span>;
+                scritta_annuncio = <span>{action}<span className="has-text-weight-bold"> {type}</span> nel corso di {nome_corso}<div className="level"><span className="level-left">{icon}</span><span className="level-right" style={style_color_time}>{notification_time}</span></div></span>
                 break;
             case 'CourseTip':
                 type = 'suggerimento'
                 link = '/courses/' + id_corso
                 icon = <span className="icon has-text-link" style={style_body_notification}><i className="fas fa-lightbulb"></i></span>;
-                scritta_annuncio = <div>{action}<span className="has-text-weight-bold"> {type}</span><div> nel corso di {nome_corso}</div><div className="level"><span className="level-left">{icon}</span><span className="level-right" style={style_color_time}>{notification_time}</span></div></div>
+                scritta_annuncio = <span>{action}<span className="has-text-weight-bold"> {type}</span> nel corso di {nome_corso}<div className="level"><span className="level-left">{icon}</span><span className="level-right" style={style_color_time}>{notification_time}</span></div></span>
                 break;
             case 'Post':
                 type = 'post'
                 link = '/publications/' + id_corso + '?post_id=' + this.props.notification.notifiable.post.id
                 icon = <span className="icon has-text-link" style={style_body_notification}><i className="fas fa-envelope"></i></span>;
-                scritta_annuncio = <div>{action}<span className="has-text-weight-bold"> {type}</span><div> nel corso di {nome_corso}</div><div className="level"><span className="level-left">{icon}</span><span className="level-right" style={style_color_time}>{notification_time}</span></div></div>
+                scritta_annuncio = <span>{action}<span className="has-text-weight-bold"> {type}</span> nel corso di {nome_corso}<div className="level"><span className="level-left">{icon}</span><span className="level-right" style={style_color_time}>{notification_time}</span></div></span>
                 break;
             case 'Rep':
                 if(this.props.notification.notifiable.rep.offer){
                     type = 'ripetizioni'
                     link = '/reps'
                     icon = <span className="icon has-text-link" style={style_body_notification}><i className="fas fa-book"></i></span>;
-                    scritta_annuncio = <div>offre <span className="has-text-weight-bold"> {type}</span>per il corso di <div>{nome_corso}</div><div className="level"><span className="level-left">{icon}</span><span className="level-right" style={style_color_time}>{notification_time}</span></div></div>
+                    scritta_annuncio = <span>offre <span className="has-text-weight-bold"> {type} </span>per il corso di {nome_corso}<div className="level"><span className="level-left">{icon}</span><span className="level-right" style={style_color_time}>{notification_time}</span></div></span>
                     break;
                 }else{
                     type = 'ripetizioni'
                     link = '/reps'
                     icon = <span className="icon has-text-link" style={style_body_notification}><i className="fas fa-book"></i></span>;
-                    scritta_annuncio = <div>cerca <span className="has-text-weight-bold"> {type}</span>per il corso di <div>{nome_corso}</div><div className="level"><span className="level-left">{icon}</span><span className="level-right" style={style_color_time}>{notification_time}</span></div></div>
+                    scritta_annuncio = <span>cerca <span className="has-text-weight-bold"> {type} </span>per il corso di {nome_corso}<div className="level"><span className="level-left">{icon}</span><span className="level-right" style={style_color_time}>{notification_time}</span></div></span>
                     break;
                 }
             case 'Document':
                 type = 'documento'
                 link = '/resources/' + id_corso + '?document_id=' + this.props.notification.notifiable.document.id
                 icon = <span className="icon has-text-link" style={style_body_notification}><i className="fas fa-file-alt"></i></span>;
-                scritta_annuncio = <div>{action}<div><span className="has-text-weight-bold"> {type}</span>per il corso di <div>{nome_corso}</div></div><div className="level"><span className="level-left">{icon}</span><span className="level-right" style={style_color_time}>{notification_time}</span></div></div>
+                scritta_annuncio = <span>{action}<span className="has-text-weight-bold"> {type} </span>per il corso di {nome_corso}<div className="level"><span className="level-left">{icon}</span><span className="level-right" style={style_color_time}>{notification_time}</span></div></span>
                 break;
             case 'Comment':
                 type = ''
                 link = '/publications/' + id_corso + '?comment_id=' + this.props.notification.notifiable.comment.id
                 icon = <span className="icon has-text-link" style={style_body_notification}><i className="fas fa-comment-alt"></i></span>;
-                scritta_annuncio = <div>ha <span className="has-text-weight-bold"> {action}</span> un post nel corso <div> di {nome_corso}</div><div className="level"><span className="level-left">{icon}</span><span className="level-right" style={style_color_time}>{notification_time}</span></div></div>
+                scritta_annuncio = <span>ha <span className="has-text-weight-bold"> {action}</span> un post nel corso di {nome_corso}<div className="level"><span className="level-left">{icon}</span><span className="level-right" style={style_color_time}>{notification_time}</span></div></span>
                 break;
         }
 
@@ -83,7 +87,8 @@ class NotificationNavbar extends React.Component {
 
         return(
 
-    <div className="navbar-item is-centered is-wrapped">
+    <div className="navbar-item columns" style={a_capo}>
+        <div className="column"></div>
         <a className="is-not-read" onClick={() => this.props.handleMarkAndRedirect(links[this.props.notification.id], this.props.notification.id)}>
         <article className="media gap">
             <figure className="media-left">
@@ -93,7 +98,7 @@ class NotificationNavbar extends React.Component {
             </figure>
             <div className="media-content">
                 <div className="content">
-                    <div style={style_dimension_tr}>
+                    <div style={style_dimension_tr} className="a_capo">
                         <div className="has-text-weight-bold">{actor}</div>
                             {scritta_annuncio}
                     </div>
@@ -101,6 +106,7 @@ class NotificationNavbar extends React.Component {
             </div>
         </article>
         </a>
+        <div className="column"></div>
     </div>
         )
     }
